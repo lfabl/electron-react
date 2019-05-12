8730a7c9f61764445d8501582d6c15a4b8e00b40
Mete ROTA <met2net3@gmail.com>
2019-05-12 06:24:44 +0300

    * Initial commit.

---
+++ public/electron.js
@@ -0,0 +1,26 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
+const isDev = require('electron-is-dev');
+let mainWindow;
+function createWindow() {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, '../build/index.html')}`);
+    mainWindow.webContents.openDevTools();
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+app.on('ready', createWindow);
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
